36e797da291ad433949e11d11c8832672e156e5a
Vadim Pavlov <vpavlov@pvm-nb.local>
2018-10-20 16:48:07 -0700

    DOH for ATC

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ATC_DOH
+
+https://ipvm.biz/atc?name=google.com
+
+# DOH Server
+https://github.com/m13253/dns-over-https/
+
+Tutorial  
+https://www.aaflalo.me/2018/10/tutorial-setup-dns-over-https-server/
---
+++ run_doh.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#ATC DOH start script
+
+SYSUSER=`whoami | awk '{print $1}'`
+DOH_ROOT="/opt/doh"
+HOST=`hostname -f`
+
+if [ ! -f $DOH_ROOT/etc/doh-server.conf ]; then
+    cat >> $DOH_ROOT/etc/doh-server.conf  << EOF
+listen = ["*:443",]
+cert = "/opt/doh/ssl/doh.crt"
+key = "/opt/doh/ssl/doh.key"
+path = "/atc"
+upstream = ["52.119.40.100:53","52.119.40.101:53",]
+timeout = 60
+tries = 10
+tcp_only = false
+verbose = false
+EOF
+
+fi
+
+if [ ! -f $DOH_ROOT/ssl/doh.crt ]; then
+	certbot-auto certonly --standalone -d $HOST -d www.$HOST
+fi
