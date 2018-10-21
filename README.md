3ba9e9422e8467c5695c793687d71746bd2df163
Vadim Pavlov <vpavlov@pvm-nb.local>
2018-10-20 17:07:17 -0700

    DOH for ATC

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 https://ipvm.biz/atc?name=google.com
 
+```
+sudo docker build -t pvmdel/atc_doh:latest .
+sudo docker run -d --name doh.ioc2rpz.com --log-driver=syslog --restart always --mount type=bind,source=/opt_data/doh,target=/opt/doh/ssl -p80:80 -p443:443 --log-opt tag="{{.ID}}/{{.Name}}" --env HOSTN="doh.ioc2rpz.com" pvmdel/atc_doh:latest	
+sudo docker exec -it d33e2363a789 bash
+```
+
 # DOH Server
 https://github.com/m13253/dns-over-https/
 
--- run_doh.sh
+++ run_doh.sh
@@ -8,8 +8,8 @@ HOST=`hostname -f`
 if [ ! -f $DOH_ROOT/etc/doh-server.conf ]; then
     cat >> $DOH_ROOT/etc/doh-server.conf  << EOF
 listen = ["*:443",]
-cert = "/opt/doh/ssl/doh.crt"
-key = "/opt/doh/ssl/doh.key"
+#cert = "/opt/doh/ssl/doh.crt"
+#key = "/opt/doh/ssl/doh.key"
 path = "/atc"
 upstream = ["52.119.40.100:53","52.119.40.101:53",]
 timeout = 60
@@ -23,3 +23,5 @@ fi
 if [ ! -f $DOH_ROOT/ssl/doh.crt ]; then
 	certbot-auto certonly --standalone -d $HOST -d www.$HOST
 fi
+
+$DOH_ROOT/bin/doh-server -conf $DOH_ROOT/etc/doh-server.conf
