22135c2a8097b18d18c941aac0a1aa709280667d
Vadim Pavlov <vpavlov@pvm-nb.local>
2019-01-18 18:14:26 -0800

    DoH proxy for ATC

--- run_doh.sh
+++ run_doh.sh
@@ -9,8 +9,16 @@ if [ -z "$PREFIX" ]; then
 	PREFIX="atc"
 fi
 
-if [ ! -f $DOH_ROOT/ssl/doh.crt ]; then
+if [ ! -f /etc/letsencrypt/live/$HOST/fullchain.pem ] && [ ! -f $DOH_ROOT/ssl/letsencrypt/live/$HOST/fullchain.pem ]; then
 	certbot -n certonly --standalone -d $HOST --agree-tos --email $EMAIL
+	cp /etc/letsencrypt/live/$HOST/fullchain.pem $DOH_ROOT/ssl/doh.crt
+	cp /etc/letsencrypt/live/$HOST/privkey.pem $DOH_ROOT/ssl/doh.key
+	cp -Rf /etc/letsencrypt $DOH_ROOT/ssl/
+elif [ ! -f /etc/letsencrypt/live/$HOST/fullchain.pem ] && [ -f $DOH_ROOT/ssl/letsencrypt/live/$HOST/fullchain.pem ]; then
+	cp -Rf $DOH_ROOT/ssl/letsencrypt /etc/
+fi
+
+if [ ! -f $DOH_ROOT/ssl/doh.crt ]; then
 	cp /etc/letsencrypt/live/$HOST/fullchain.pem $DOH_ROOT/ssl/doh.crt
 	cp /etc/letsencrypt/live/$HOST/privkey.pem $DOH_ROOT/ssl/doh.key
 fi
@@ -30,7 +38,7 @@ EOF
 
 	cat >> /tmp/$SYSUSER  << EOF
 ### Renew certificates
-42 0,12 * * * python -c 'import random; import time; time.sleep(random.random() * 3600)' && /usr/bin/certbot renew && if ! cmp -s /etc/letsencrypt/live/$HOST/fullchain.pem $DOH_ROOT/ssl/doh.crt; then cp /etc/letsencrypt/live/$HOST/fullchain.pem $DOH_ROOT/ssl/doh.crt && cp /etc/letsencrypt/live/$HOST/privkey.pem $DOH_ROOT/ssl/doh.key && killall -9 doh-server; fi
+42 0,12 * * * /usr/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && /usr/bin/certbot renew && if ! cmp -s /etc/letsencrypt/live/$HOST/fullchain.pem $DOH_ROOT/ssl/doh.crt; then cp /etc/letsencrypt/live/$HOST/fullchain.pem $DOH_ROOT/ssl/doh.crt && cp /etc/letsencrypt/live/$HOST/privkey.pem $DOH_ROOT/ssl/doh.key && killall -9 doh-server; fi
 EOF
 
 fi
